4cc5ba54a901d2bd140c8bc05c1dafe99dafcd81
Jasmine Dang <ngandth.1992@gmail.com>
2021-01-17 18:06:04 +0800

    get users with saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,15 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/index.js';
+import rootSaga from './sagas/index.js';
 
-const store = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = compose(
+  applyMiddleware(sagaMiddleware),
+  window.devToolsExtension && window.devToolsExtension()
+)(createStore)(rootReducer);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
