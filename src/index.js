035f94e52aeb33b4a9ad43afe08774406bc11c41
Jasmine Dang <ngandth.1992@gmail.com>
2021-01-17 12:01:50 +0800

    display user card;

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
-import store from 'redux/store.js';
+import store from './redux/store.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
